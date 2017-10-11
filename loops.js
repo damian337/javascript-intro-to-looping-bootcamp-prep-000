24996e6bda6b3ab00cb4783abf0e2901b4497e1a
Damian Guillory <guillorywdamian@gmail.com>
2017-10-11 00:46:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,9 @@
 for (var i = 0; i < 25; i++) {
-  console.log( "Hello World the " + i + " time" );
+    if (i === 1) {
+        console.log("I am 1 strange loop.");
+    }
+
+    else {
+        console.log("I am " + i + "strange loops.");
+    }
 }
